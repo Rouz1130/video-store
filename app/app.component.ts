48a91e127ead891405a2e6e1e53d75b670d08688
Joel and Rouz <joel+rouz@>
2016-08-25 10:40:52 -0700

    set up initial files, connect app.component.ts to index.html, connect CSS file and Bootstrap

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <img src="resources/images/logo.jpg" />
+      <h1>Video Store</h1>
+    </div>
+    `
+})
+export class AppComponent {
+
+}
---
+++ app/video.component.ts
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'video-display',
+  template: `
+    <h2>Also Connected</h2>
+    `
+})
+export class VideoComponent {
+  
+}
